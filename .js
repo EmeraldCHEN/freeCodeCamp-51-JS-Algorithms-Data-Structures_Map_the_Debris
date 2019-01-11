773bfdf1b1c765084da9e01f98a0803fc6c1452e
Emerald Chen <ahyuancy@gmail.com>
2019-01-11 18:59:18 +1300

    Update .js

--- .js
+++ .js
@@ -18,8 +18,7 @@ function orbitalPeriod(arr) {
 }
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]); // return [{name: "sputnik", orbitalPeriod: 86400}].
 
-orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, 
-                {name: "moon", avgAlt: 378632.553}]);
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
 // return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}]
 
 
